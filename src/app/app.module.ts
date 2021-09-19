d01381baa2d6c47e47cbcfda9928fc632b7cf4ba
coriccf <corina.colque.f@gmail.com>
2021-09-19 02:55:24 -0400

    add components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
+import { RouterModule, Routes } from '@angular/router';
+
+
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
 
+const routes: Routes = [
+  { path: 'contactos', component: ContactosComponent  },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+
+
+];
 
 @NgModule({
   declarations: [
+    
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ContactosComponent
 
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
+   
+
   ],
   providers: [],
   bootstrap: [AppComponent]
