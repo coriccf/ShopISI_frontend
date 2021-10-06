0f500db7d63721e56a14b13ce2c201a122c7b270
coriccf <corina.colque.f@gmail.com>
2021-10-06 10:33:21 -0400

    Register user

--- src/app/components/profile-view/profile-view.component.ts
+++ src/app/components/profile-view/profile-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { User } from "./user";
+import { UserService } from "./user.service";
 
 @Component({
   selector: 'app-profile-view',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileViewComponent implements OnInit {
 
-  constructor() { }
+  filterPost='';
+
+  user!: User[];
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getAll().subscribe(
+      u => this.user=u
+    );
+  }
+
+  delete(user:User):void{
+    console.log("Delete");
+    this.userService.delete(user.user_id).subscribe(
+      res=>this.userService.getAll().subscribe(
+        response=> this.user=response
+      )
+    );
+  }
+
+  update(user:User){
+    console.log("Update");
+    this.userService.update(user).subscribe(
+      res=>this.userService.getAll().subscribe(
+        response=>this.user=response
+      )
+    );
   }
 
 }
--- src/app/components/sesion/sesion.component.ts
+++ src/app/components/sesion/sesion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from "@angular/router";
+import { User } from "./user";
+import { UserService } from "./user.service";
 @Component({
   selector: 'app-sesion',
   templateUrl: './sesion.component.html',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SesionComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+    user_id: number=0;
+    name: string ="";
+    lastname: string="";
+    email: string="";
+    cellphone: string="";
+    telephone: string="";
+    username: string="";
+    password:string="";
+    rol:string="";
+    status: number=0;
+  
+  user:User=new User(this.user_id, this.name,this.lastname,this.email,this.cellphone,this.telephone,this.username,this.password,this.rol,this.status);
+  
+    constructor(private userService:UserService, private router:Router, private activatedRoute:ActivatedRoute ) { }
+  
+    ngOnInit(): void {
+      this.cargar();
+    }
+  
+    cargar():void{
+      this.activatedRoute.params.subscribe(
+        e=>{
+          let id=e['id'];
+              if(id){
+                this.userService.get(id).subscribe(
+                  es=> this.user=es
+                );
+              }
+        }
+      )
+    }
+  
+    create():void{
+      console.log(this.user);
+      this.userService.create(this.user).subscribe(
+        res=> this.router.navigate(['/user'])
+      );
+    }
+  
+    update():void{
+      this.userService.update(this.user).subscribe(
+        e=>this.router.navigate(['/user'])
+      );
+    }
+  
+  
+  
   }
-
-}
