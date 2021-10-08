7a3589255ea9e9b5a69fe0725427ebb4264c2372
coriccf <corina.colque.f@gmail.com>
2021-10-08 17:13:26 -0400

    add edit

--- src/app/components/client-edit/client-edit.component.ts
+++ src/app/components/client-edit/client-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from "@angular/router";
+import { Client } from "./client";
+import { ClientService } from "./client.service";
 @Component({
   selector: 'app-client-edit',
   templateUrl: './client-edit.component.html',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientEditComponent implements OnInit {
 
-  constructor() { }
+
+  client_id: number=0;
+  name: string ="";
+  lastname: string="";
+  email: string="";
+  cellphone: string="";
+  telephone: string="";
+  address: string="";
+  clientname: string="";
+  password:string="";
+  
+
+client:Client=new Client(this.client_id, this.name,this.lastname,this.email,this.cellphone,this.telephone,this.address,this.clientname,this.password);
+
+  constructor(private clientService:ClientService, private router:Router, private activatedRoute:ActivatedRoute ) { }
 
   ngOnInit(): void {
+    this.cargar();
   }
+
+  cargar():void{
+    this.activatedRoute.params.subscribe(
+      e=>{
+        let id=e['id'];
+            if(id){
+              this.clientService.get(id).subscribe(
+                es=> this.client=es
+              );
+            }
+      }
+    )
+  }
+
+  create():void{
+    console.log(this.client);
+    this.clientService.create(this.client).subscribe(
+      res=> this.router.navigate(['/client'])
+    );
+  }
+
+  update():void{
+    this.clientService.update(this.client).subscribe(
+      e=>this.router.navigate(['/client'])
+    );
+  }
+
+
 
 }
