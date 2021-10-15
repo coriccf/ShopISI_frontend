c49a2d1d5b15457f84e89b50fc684a4e37b2764c
lr-salinas <laura.salinas.r@ucb.edu.bo>
2021-10-14 23:43:42 -0400

    adding details in card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from './card';
+import { CardService } from './card.service';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  filterPost='';
 
-  constructor() { }
+  card!: Card[];
+
+  constructor(private cardService: CardService) { }
 
   ngOnInit(): void {
+    this.cardService.getAll().subscribe(
+      u => this.card=u
+    );
   }
+
+  delete(card:Card):void{
+    console.log("Delete");
+    this.cardService.delete(card.card_id).subscribe(
+      res=>this.cardService.getAll().subscribe(
+        response=> this.card=response
+      )
+    );
+  }
+
+  update(card: Card){
+    console.log("Update");
+    this.cardService.update(card).subscribe(
+      res=>this.cardService.getAll().subscribe(
+        response=>this.card=response
+      )
+    );
+  }
+
+  create(card:Card){
+    console.log("Create");
+    this.cardService.create(card).subscribe(
+      res=>this.cardService.getAll().subscribe(
+        response=>this.card=response
+      )
+    );
+
+  }
+
 
 }
--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -6,10 +6,10 @@ export class Card{
   expiration_year: number=0;
   expiration_month: number=0;
   cvc: number=0;
-  creation_date!: Date;
+  //creation_date!: Date;
   status: number=0;
 
-  constructor(card_id:number, user_id: number, card_name:string, card_number:number,expiration_year:number, expiration_month:number, cvc:number,creation_date: Date,status:number) {
+  constructor(card_id:number, user_id: number, card_name:string, card_number:number,expiration_year:number, expiration_month:number, cvc:number,status:number) {
     this.card_id = card_id;
     this.user_id = user_id;
     this.card_name = card_name;
@@ -17,7 +17,7 @@ export class Card{
     this.expiration_year = expiration_year;
     this.expiration_month = expiration_month;
     this.cvc = cvc;
-    this.creation_date = creation_date;
+   // this.creation_date = creation_date;
     this.status = status;
 }
 }
