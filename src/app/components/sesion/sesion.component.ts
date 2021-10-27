49cf7a4adcc6f05f12db54860f59750b7bb1d72a
coriccf <corina.colque.f@gmail.com>
2021-10-27 08:15:18 -0400

    Fixing user rol client

--- src/app/components/sesion/sesion.component.ts
+++ src/app/components/sesion/sesion.component.ts
@@ -1,59 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import { Client } from "./client";
-import { ClientService } from "./client.service";
+import { User } from "./user";
+import { UserService } from "./user.service";
+
+
+
+
 @Component({
   selector: 'app-sesion',
   templateUrl: './sesion.component.html',
   styleUrls: ['./sesion.component.css']
 })
 export class SesionComponent implements OnInit {
+  user_id: number=0;
+  rol_id: number=0
+  name: string ="";
+  lastname: string="";
+  email: string="";
+  cellphone: string="";
+  telephone: string="";
+  username: string="";
+  password:string="";
+  rol:string="";
+  status: number=0;
+  
 
-    client_id: number=0;
-    name: string ="";
-    lastname: string="";
-    email: string="";
-    cellphone: string="";
-    telephone: string="";
-    address: string="";
-    clientname: string="";
-    password:string="";
-    
-  
-  client:Client=new Client(this.client_id, this.name,this.lastname,this.email,this.cellphone,this.telephone,this.address,this.clientname,this.password);
-  
-    constructor(private clientService:ClientService, private router:Router, private activatedRoute:ActivatedRoute ) { }
-  
-    ngOnInit(): void {
-      this.cargar();
-    }
-  
-    cargar():void{
-      this.activatedRoute.params.subscribe(
-        e=>{
-          let id=e['id'];
-              if(id){
-                this.clientService.get(id).subscribe(
-                  es=> this.client=es
-                );
-              }
-        }
-      )
-    }
-  
-    create():void{
-      console.log(this.client);
-      this.clientService.create(this.client).subscribe(
-        res=> this.router.navigate(['/client'])
-      );
-    }
-  
-    update():void{
-      this.clientService.update(this.client).subscribe(
-        e=>this.router.navigate(['/client'])
-      );
-    }
-  
-  
-  
+user:User=new User(this.user_id,this.rol_id,this.name,this.lastname,this.email,this.cellphone,this.telephone,this.username,this.password,this.rol,this.status);
+
+  constructor(private userService:UserService, private router:Router, private activatedRoute:ActivatedRoute ) { }
+
+  ngOnInit(): void {
+    this.cargar();
   }
+
+  cargar():void{
+    this.activatedRoute.params.subscribe(
+      e=>{
+        let id=e['id'];
+            if(id){
+              this.userService.get(id).subscribe(
+                es=> this.user=es
+              );
+            }
+      }
+    )
+  }
+
+  create():void{
+    console.log(this.user);
+    this.userService.create(this.user).subscribe(
+      res=> this.router.navigate(['/user'])
+    );
+  }
+
+  update():void{
+    this.userService.update(this.user).subscribe(
+      e=>this.router.navigate(['/user'])
+    );
+  }
+
+
+
+}
