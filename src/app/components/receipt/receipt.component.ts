067f54f53dec7e4446d3e16f5ef4617959cd9115
coriccf <corina.colque.f@gmail.com>
2021-11-02 19:10:30 -0400

    update receipt

--- src/app/components/receipt/receipt.component.ts
+++ src/app/components/receipt/receipt.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Receipt } from './receipt';
 import { ReceiptService } from './receipt.service';
-import { Product } from '../products/product';
-import { ProductService } from '../products/product.service';
-import { User } from '../user/user-admin/user';
-import { UserService } from '../user/user-admin/user.service';
-import { Card } from '../card/card';
-import { CardService } from '../card/card.service';
+
 
 
 @Component({
@@ -19,31 +14,12 @@ export class ReceiptComponent implements OnInit {
   filterPost='';
 
   receipt!: Receipt[];
-  product: Array<Product>=[];
-  user: Array<User>=[];
-  card: Array<Card>=[];
- 
 
-  
-  constructor(private productService:ProductService, private receiptService:ReceiptService, private userService:UserService,private cardService:CardService ) { }
+  constructor( private receiptService:ReceiptService) { }
   ngOnInit(): void {
-    this.productService.getAll().subscribe(
-      p => this.products=p
-    );
+  
     this.receiptService.getAll().subscribe(
       a => this.receipt=a
     );
-    this.userService.getAll().subscribe(
-      b => this.user=b
-    );
-    this.userService.getAll().subscribe(
-      c => this.user=c
-    );
-    this.userService.getAll().subscribe(
-      d => this.user=d
-    );
-
-    
-
   }
 }
--- src/app/components/receipt/receipt.ts
+++ src/app/components/receipt/receipt.ts
@@ -1,19 +1,19 @@
 export class Receipt {
 
     receipt_id: number=0;
-    product_id: number=0;
-    user_id: number=0;
-    card_id: number=0;
-    cart_id: number=0;
+    name: String= " ";
+    lastname: String= " ";
+    product_name: String= " ";
+    cost_product: number=0;
     date: string="";
 
-    constructor(receipt_id:number,product_id:number,user_id:number, address_id:number,cart_id:number, date:string){
+    constructor(receipt_id:number,name:string, lastname:string, product_name:string, cost_product:number, date:string){
         // ,imagen_id:number[]
         this.receipt_id=receipt_id;
-        this.product_id=product_id;
-        this.user_id=user_id;
-        this.card_id=cart_id;
-        this.cart_id=cart_id;
+        this.name=name;
+        this.lastname=lastname;
+        this.product_name=product_name;
+        this.cost_product=cost_product;
         this.date=date;
     }
 }
